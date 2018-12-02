45b63401f4e86b404679b663f37db8b8896f8e5b
Michael Klishevich <busation@gmail.com>
2018-12-02 16:28:43 +0300

    feat(redux): add redux to project

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Type from 'prop-types';
 import { Link } from 'react-router-dom';
 import elbrusImg from './elbrus.png';
 import { PAGES } from '../../routes/pages';
 import { bemClassNameFactory } from '../../utils/bem';
+import { sayByeAC, sayHiAC } from '../../redux/actions/app-actions';
+import { selectSay } from '../../redux/selectors/app-selectors';
 import './app.css';
 
 const cn = bemClassNameFactory('app');
 
-export default class App extends Component {
+const mapStateToProps = state => ({
+  say: selectSay(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  sayBye: () => dispatch(sayByeAC()),
+  sayHi: () => dispatch(sayHiAC())
+});
+
+class App extends Component {
   static propTypes = {
     appName: Type.string,
-    children: Type.node.isRequired
+    children: Type.node.isRequired,
+    sayHi: Type.func,
+    sayBye: Type.func,
+    say: Type.string
   };
 
   static defaultProps = {
@@ -24,21 +39,27 @@ export default class App extends Component {
 
   handleClickButton = () => {
     const { buttonActive } = this.state;
-    console.log(buttonActive);
     this.setState({ buttonActive: !buttonActive });
+  };
+
+  handleClickSayHi = () => {
+    this.props.sayHi();
+  };
+
+  handleClickSayBye = () => {
+    this.props.sayBye();
   };
 
   componentDidMount() {
     const fetchFunc = async () => {
       const res = await fetch('/api/test');
-      console.log(res);
       return res;
     };
     fetchFunc();
   }
 
   render() {
-    const { appName, children } = this.props;
+    const { appName, children, say } = this.props;
     console.log(this.props);
     return (
       <div className={ cn() }>
@@ -62,6 +83,28 @@ export default class App extends Component {
               Click Me
             </button>
           </div>
+          <div className={ cn('button-block') }>
+            <h2>Buttons Redux</h2>
+            <div>
+              <button
+                className={ cn('button', 'red') }
+                onClick={ this.handleClickSayHi }
+              >
+                Say Hi
+              </button>
+            </div>
+            <div>
+              <button
+                className={ cn('button', 'red') }
+                onClick={ this.handleClickSayBye }
+              >
+                Say Bye
+              </button>
+            </div>
+            <div>
+              { say }
+            </div>
+          </div>
         </div>
         { children }
         <div className={ cn('footer') }>
@@ -70,3 +113,9 @@ export default class App extends Component {
     );
   }
 }
+
+const VisibleApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+export default VisibleApp;
--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Router } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './redux/reducers';
 import Routes from './routes/routes';
 
 const history = createBrowserHistory();
+const composeEnhancers = composeWithDevTools({});
+const initialState = {
+  app: {
+    name: 'Express React Skeleton',
+    say: 'nothing for now'
+  }
+};
+
+const store = createStore(reducers, initialState, composeEnhancers());
 
 const Index = () => (
-  <Router history={history}>
-    <Routes />
-  </Router>
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Routes />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(<Index />, document.getElementById('react-app'));
